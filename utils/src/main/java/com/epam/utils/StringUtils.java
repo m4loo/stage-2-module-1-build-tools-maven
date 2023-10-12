9abdf4dcddcb665019c7503eb17321f157c86c99
m4loo <128167781+m4loo@users.noreply.github.com>
2023-10-12 18:01:05 +0600

    done?

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,10 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+        try{
+            return Double.parseDouble(str)>0;
+        }catch (NumberFormatException |NullPointerException e){
+            return false;
+        }
     }
 }
